1954c60819f1c32ebe56eeecce8980b2e6aed61f
Arun Ahuja <aahuja11@gmail.com>
2016-09-15 12:48:18 -0400

    fix filters and NA values

--- pygdc/cohort.py
+++ pygdc/cohort.py
@@ -1,9 +1,11 @@
 from cohorts import Cohort, Patient
+import logging
 import json
+
 
 from .api import get_cases
 from .filters import and_filter, equals_filter
-from .fields import CASE_DEFAULT_FIELDS, CASE_DIAGNOSIS_FIELDS, CASE_SAMPLE_FIELDS, CASE_EXPOSURE_FIELDS
+from .fields import CASES_DEFAULT_FIELDS, CASES_DIAGNOSES_FIELDS, CASES_SAMPLE_FIELDS, CASES_EXPOSURE_FIELDS
 
 def build_cohort(primary_site,
                  cohort_cache_dir,
@@ -20,42 +22,54 @@ def build_cohort(primary_site,
             equals_filter('project.primary_site', primary_site),
             equals_filter('project.diesase_type', disease_type) if disease_type else None,
             equals_filter('project.program.name', 'TCGA') if only_tcga else None,
-            **[equals_filter(k, v) for (k, v) in kwargs]
+            *[equals_filter(k, v) for (k, v) in kwargs.items()]
         )
     )
 
-    filter_fields = ['project.primary_site'] + kwargs.keys()
+    filter_fields = ['project.primary_site'] + list(kwargs.keys())
 
     # Automatically include the filter fields in the output
-    fields = list(CASE_DEFAULT_FIELDS) + filter_fields
+    fields = list(CASES_DEFAULT_FIELDS) + filter_fields
 
     if with_diagnosis: 
-        fields += CASE_DIAGNOSIS_FIELDS
+        fields += CASES_DIAGNOSES_FIELDS
     if with_samples: 
-        fields += CASE_SAMPLE_FIELDS
+        fields += CASES_SAMPLE_FIELDS
     if with_exposures: 
-        fields += CASE_EXPOSURE_FIELDS
+        fields += CASES_EXPOSURE_FIELDS
 
     results = get_cases(filters=filters, fields=','.join(fields))
 
     patients = []
     for result in results:
         submitter_id = result['submitter_id']
-        diagnoses = result['diagnoses']
+   
+        if 'diagnoses' in result:
+            diagnoses = result['diagnoses']
 
-        assert len(diagnoses) == 1, "Patient {} had multiple diagnoses".format(submitter_id)
-        diagnosis = result['diagnoses'][0]
-        deceased = diagnosis['vital_status'] == 'dead'
-        progressed = diagnosis['progression_or_recurrence'] == 'progression'
-        patient = Patient(
-                id=submitter_id,
-                deceased=deceased,
-                progressed=progressed,
-                os=diagnosis['days_to_death'] if deceased else diagnosis['days_to_last_follow_up'],
-                pfs=diagnosis['days_to_recurrence'] if progressed else diagnosis['days_to_last_follow_up'],
-                benefit=False,
-            )
-        patients.append(patient)
+            assert len(diagnoses) == 1, "Patient {} had multiple diagnoses".format(submitter_id)
+            
+            diagnosis = diagnoses[0]
+            deceased = diagnosis['vital_status'] == 'dead'
+            progressed = diagnosis['progression_or_recurrence'] == 'progression'
+            censor_time = diagnosis['days_to_last_follow_up']
+            censor_time = censor_time or 0
+
+            os = (diagnosis['days_to_death'] if deceased else censor_time) or 0 # TODO: support NA
+            pfs = (diagnosis['days_to_recurrence'] if progressed else censor_time) or 0 # TODO: support NA
+
+            patient = Patient(
+                    id=submitter_id,
+                    deceased=deceased,
+                    progressed=progressed,
+                    os=os,
+                    pfs=max(pfs, os),
+                    benefit=False,
+                    additional_data=result,
+                )
+            patients.append(patient)
+        else:
+            logging.warn("No diagnoses found for {}".format(submitter_id))
 
     cohort = Cohort(
         patients=patients,
